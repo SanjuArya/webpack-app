6e116a5a53d63ef1e86a9797697d70165967d307
Sanjay <sanjayaryadev@gmail.com>
2019-07-16 18:12:03 +0530

    Changed style.css to style.scss, installed BootStrap and sass-loader, imported BootStrap in style.scss and used 'sass-loader' to compile scss to css

--- src/engine.js
+++ src/engine.js
@@ -1,6 +1,6 @@
 import { Sanju } from './js/Sanju'
 import { Deepu } from './js/Deepu'
 import { Suraj } from './js/Suraj'
-import css from './css/style.css'
+import css from './scss/style.scss'
 
 document.write( `${ Sanju() } ${ Deepu() } ${ Suraj() }` )
